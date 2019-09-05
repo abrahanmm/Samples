f723424ea9d923ee50d2495749301a613fbdce35
Abrahán <abrahanmm@gmail.com>
2019-09-05 12:49:48 +0200

    Add swagger

--- NFLGameWeather/NFLGameWeather/Startup.cs
+++ NFLGameWeather/NFLGameWeather/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NFLGameWeather.Model.Services;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace NFLGameWeather
 {
@@ -27,7 +29,13 @@ namespace NFLGameWeather
         {
             services.AddScoped<IGameWeatherService, GameWeatherService>();
             services.AddScoped<IForecastService, ForecastService>();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "NFL Game Wheather Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +45,18 @@ namespace NFLGameWeather
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "NFL Game Wheather");
+                c.RoutePrefix = string.Empty;
+                c.DefaultModelRendering(ModelRendering.Model);
+            });
 
             app.UseMvc();
         }
